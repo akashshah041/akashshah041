dbfd37fe54c202330528ca1ba818576f0fbe100a
Akashkumar Shah <Akashkumar.Shah@dpworld.com>
2024-06-28 14:44:17 +0800

    hooks + setup_config file

--- setup_config.sh
+++ setup_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the post-checkout hook to the .git/hooks directory
-cp hooks/post-checkout .git/hooks/
+cp hooks/post-checkout  .git/hooks/
 chmod +x .git/hooks/post-checkout
 
 # Set the custom hooks path for the current branch
